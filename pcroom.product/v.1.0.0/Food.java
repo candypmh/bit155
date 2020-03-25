67f15a60521ae1ed7d9291dbc8e8818e72106c0e
candypmh <61579166+candypmh@users.noreply.github.com>
2020-03-25 12:16:32 +0900

    2020.03.24

---
+++ pcroom.product/v.1.0.0/Food.java
@@ -0,0 +1,20 @@
+package pcroom.product;
+
+public class Food extends Product{
+
+
+	Food(int price, String pname) {
+		super(price, pname);
+		
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Food";
+	}
+	
+	
+
+}
