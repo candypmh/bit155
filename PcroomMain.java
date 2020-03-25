b88cb0e1f6ebf851842a78d30595de4cc89c9710
candypmh <61579166+candypmh@users.noreply.github.com>
2020-03-25 12:02:45 +0900

    v1.0.0
    가장 초기버전

---
+++ PcroomMain.java
@@ -0,0 +1,16 @@
+package pcroom;
+
+import pcroom.user.Customer;
+
+public class PcroomMain {
+	public static void main(String[] args) {
+		Internet_Cafe ic = new Internet_Cafe();
+		ic.showMain();
+		
+		
+	}
+	
+
+}
+
+
