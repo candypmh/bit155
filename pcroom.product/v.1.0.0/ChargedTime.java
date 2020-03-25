6b7bc5883bd456abe625b5af28fbbec89d0799a5
candypmh <61579166+candypmh@users.noreply.github.com>
2020-03-25 12:15:41 +0900

    2020.03.24

---
+++ pcroom.product/v.1.0.0/ChargedTime.java
@@ -0,0 +1,18 @@
+package pcroom.product;
+
+public class ChargedTime extends Product{
+
+	ChargedTime(int price, String pname) {
+		super(price, pname);
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Time";
+	}
+
+	
+	
+	
+}
