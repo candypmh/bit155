8dde95ff709ba2dc77478456a2c62bea9e2d6ccb
candypmh <61579166+candypmh@users.noreply.github.com>
2020-03-25 12:16:08 +0900

    2020.03.24

---
+++ pcroom.product/v.1.0.0/Drink.java
@@ -0,0 +1,26 @@
+package pcroom.product;
+
+public class Drink extends Product{
+
+	
+	
+	Drink(int price, String pname) {
+		super(price, pname);
+
+	}
+	
+	@Override
+	public String toString() {
+		return "Drink";
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+}
